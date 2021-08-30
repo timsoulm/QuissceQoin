95839fb83bbe6a51b33f3113c6e15b4597ece329
Adam <a.bednarz.levine@gmail.com>
2021-08-30 23:57:25 +0300

    add fallback behavior

--- src/App.js
+++ src/App.js
@@ -75,7 +75,12 @@ function App() {
   }, []);
 
   if (!web3) {
-    return <div />;
+    return <div style={{ display: "flex", flexDirection: "column" }}>
+      <h4>
+        This browser doesn't support web3. Please use a desktop browser with a tool such as <a href="https://metamask.io/">Metamask</a> to use the Quissce Qoin Execution Console™.
+      </h4>
+      <img src="sadhorse.jpg" />
+    </div>;
   }
 
   let activeTab = <div />;
